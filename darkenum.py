25f073094319d3b94310459d2b0b0defd773ba9d
Ohmjones <Shane.Jones@Ohmsecurities.com>
2016-10-18 20:19:51 -0400

    Update darkenum.py

--- darkenum.py
+++ darkenum.py
@@ -107,21 +107,132 @@ def unicorn(ip_address):
 	tcpport_dict = tcpports
 
 # populate udp service names & ports
-	udpports = []
+#!/usr/bin/python
+# Script to enumerate a given list of livehosts
+
+# Module Importations
+import subprocess, multiprocessing, os, time, re
+from multiprocessing import Process, Queue
+
+# Kick off multiprocessing
+def xProc(targetin, target, port):
+	jobs = []
+	proc = multiprocessing.Process(target=targetin, args=(target,port))
+	jobs.append(proc)
+	proc.start()
+	return
+
+# Kick off further enumeration.
+def http(ip_address, port):
+	print "[*] Launching HTTP scripts on " + ip_address
+	httpscript = "~/Scripts/http.py %s" % (ip_address)
+	os.system("gnome-terminal -e 'bash -c \"" + httpscript + "\";bash'")
+	return
+
+def https(ip_address, port):
+	print "[*] Launching HTTPS scripts on " + ip_address
+	httpsscript = "~/Scripts/https.py %s" % (ip_address)
+	os.system("gnome-terminal -e 'bash -c \"" + httpsscript + "\";bash'")	
+     	return
+
+def mssql(ip_address, port):
+	print "[*] Launching MSSQL scripts on " + ip_address
+	mssqlscript = "~/Scripts/mssql.py %s" % (ip_address)
+	os.system("gnome-terminal -e 'bash -c \"" + mssqlscript + "\"'")
+     	return
+
+def mysql(ip_address, port):
+	print "[*] Launching MYSQL scripts on " + ip_address
+	mysqlscript = "~/Scripts/mysql.py %s" % (ip_address)
+	os.system("gnome-terminal -e 'bash -c \"" + mysqlscript + "\"'")
+     	return    
+
+def ssh(ip_address, port):
+	print "[*] Launching SSH scripts on " + ip_address
+	sshscript = "~/Scripts/ssh.py %s" % (ip_address)
+	os.system("gnome-terminal -e 'bash -c \"" + sshscript + "\";bash'")
+	return
+
+def snmp(ip_address, port):
+	print "[*] Launching SNMP scripts on " + ip_address   
+	snmpscript = "~/Scripts/snmp.py %s" % (ip_address)
+	os.system("gnome-terminal -e 'bash -c \"" + snmpscript + "\"'") 
+	return
+
+def smtp(ip_address, port):
+	print "[*] Launching SMTP scripts on " + ip_address 
+	smtpscript = "~/Scripts/smtp.py %s" % (ip_address)
+	os.system("gnome-terminal -e 'bash -c \"" + smtpscript + "\";bash'")
+	return
+
+def samba(ip_address, port):
+	print "[*] Launching SAMBA scripts on " + ip_address
+	sambascript = "~/Scripts/samba.py %s" % (ip_address)
+	os.system("gnome-terminal -e 'bash -c \"" + sambascript + "\"'")
+	return
+
+def intrusive(ip_address):
+	print "[*] Unicornscan on " + ip_address + " completed. Running Intrusive NMAP scans against target."
+	cmd = "nmap -Pn -sTU -pT:" + ','.join(tcpport_dict) + ",U:" + ','.join(udpport_dict) + " --open -sC -sV -O %s -oA /tmp/%s/intrusivescan" % (ip_address, ip_address)
+	os.system("gnome-terminal -e 'bash -c \"" + cmd + "\"'")	
+
+def unicorn(ip_address):
+	ip_address = ip_address.strip()
+	print "[*] Running initial TCP/UDP fingerprinting on " + ip_address + " [*]"
+	global tcpport_dict
+	global tcpserv_dict
+	global udpport_dict
+	global udpserv_dict
+	tcpport_dict = {}
+	tcpserv_dict = {}
+	udpport_dict = {}
+	udpserv_dict = {}
+	usout = open('/tmp/' + ip_address + '/unicorn','w')
+
+	#tcp scan  -p1-65535
+	tcptest = "unicornscan -mT -r500 -I %s" % ip_address
+	calltcpscan = subprocess.Popen(tcptest, stdout=subprocess.PIPE, shell=True)
+	calltcpscan.wait()
+	#udp scan  -p1-65535
+	udptest = "unicornscan -mU -r500 -I %s" % ip_address
+	calludpscan = subprocess.Popen(udptest, stdout=subprocess.PIPE, shell=True)
+	calludpscan.wait()
+
+# populate tcp service names & ports
+	tcpservice = []
+	tcpports = []
+	for lines in calltcpscan.stdout:
+		if re.search('\[([\s0-9{1,5}]+)\]',lines):
+			linez = re.split('\s', lines)
+			cleansrv = filter(None, linez)
+			services = [cleansrv.strip('[') for cleansrv in cleansrv][2]
+			cleanprt = filter(None, linez)
+			ports = [cleanprt.strip(']') for cleanprt in cleanprt][3]
+			#print services + ":" + ports
+			tcpservice.append(services)
+			tcpports.append(ports)
+
+	tcpserv_dict = tcpservice
+	tcpport_dict = tcpports
+	usout.write(str(tcpserv_dict) + ":" + str(tcpport_dict) + '\n')
+
+# populate udp service names & ports
 	udpservice = []
+	udpports = []
 	for lines in calludpscan.stdout:
 		if re.search('\[([\s0-9{1,5}]+)\]',lines):
-			linez = re.split('\W+', lines)
-			services = [x for x in linez if x][2]
-			ports = [x for x in linez if x][3]
-			for data in zip(services, ports):
-				if services not in udpservice:
-					udpservice.append(services)
-				if ports not in udpports:
-					udpports.append(ports)
-				usout.write(str(udpservice) + ":" + str(udpports) + '\n')
+			linez = re.split('\s', lines)
+			cleansrv = filter(None, linez)
+			services = [cleansrv.strip('[') for cleansrv in cleansrv][2]
+			cleanprt = filter(None, linez)
+			ports = [cleanprt.strip(']') for cleanprt in cleanprt][3]
+			#print services + ":" + ports
+			udpservice.append(services)
+			udpports.append(ports)
+
 	udpserv_dict = udpservice
 	udpport_dict = udpports
+	usout.write(str(udpserv_dict) + ":" + str(udpport_dict) + '\n')
 	usout.close()
 	
 # Kick off intrusive Nmap scanning
@@ -167,15 +278,15 @@ def unicorn(ip_address):
 			time.sleep(60)
 			xProc(mysql, ip_address, None)			
 
-		for service, port in zip(udpserv_dict,udpport_dict):
-			if (service == "snmp") and (port == "161"):
-				print "[!] Detected snmp on " + ip_address + ":" + port + " (UDP)"
-				time.sleep(180)
-				xProc(snmp, ip_address, None)
-			elif (service == "netbios") and (port == "137") or (port == "138"):
-				print "[!] Netbios detected on UDP. If nmap states the tcp port is vulnerable, run '-pT:445,U:137' to eliminate false positive"
-			else:
-				print "[*] Nmap intrusive scan output should be thoroughly reviewed at /tmp/" + ip_address
+	for service, port in zip(udpserv_dict,udpport_dict):
+		if (service == "snmp") and (port == "161"):
+			print "[!] Detected snmp on " + ip_address + ":" + port + " (UDP)"
+			time.sleep(180)
+			xProc(snmp, ip_address, None)
+		elif (service == "netbios") and (port == "137") or (port == "138"):
+			print "[!] Netbios detected on UDP. If nmap states the tcp port is vulnerable, run '-pT:445,U:137' to eliminate false positive"
+		else:
+			print "[*] Nmap intrusive scan output should be thoroughly reviewed at /tmp/" + ip_address
 
 print "############################################################"
 print "####                                                    ####"
@@ -184,6 +295,15 @@ print "####                      by: Ohm                       ####"
 print "############################################################"
  
 if __name__=='__main__':
+	targetfile = open('/tmp/livehosts', 'r')		
+	for target in targetfile:
+		path = os.path.join("/tmp", target.strip())
+		try:		
+			os.mkdir(path)
+		except:
+			pass
+		unicorn(target)	
+	targetfile.close()
 	targetfile = open('/tmp/livehosts', 'r')		
 	for target in targetfile:
 		path = os.path.join("/tmp", target.strip())
