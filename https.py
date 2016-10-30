92ff31f1746fce34dbf0d90accd108a2ff024c49
Ohmjones <Shane.Jones@Ohmsecurities.com>
2016-10-30 16:49:39 -0400

    Update https.py

--- https.py
+++ https.py
@@ -11,38 +11,49 @@ wordlists = ["/usr/share/dirb/wordlists/big.txt", "/usr/share/dirb/wordlists/vul
 wfuzzlist = ["/usr/share/wfuzz/wordlist/general/big.txt", "/usr/share/wfuzz/wordlist/vulns/cgis.txt"]
 
 def gobuster(url):
-	print "[!] Starting gobuster scan for " + url
 	if ("big" in wordlists[0]):
-		time.sleep(15)
-		gobuster = "gobuster -u " + url + " -w " +  str(wordlists[0]) + " -s '200,204,301,302,307,403,500'"
-		os.system("gnome-terminal -e 'bash -c \"" + gobuster + "\";bash'")
+		print "[!] Starting gobuster big scan for " + url
+		gobuster = "gobuster -u " + url + " -w " +  str(wordlists[0]) + " -s '200,204,301,302,307,403,400'"
+		callbigscan = subprocess.Popen(gobuster, stdout=subprocess.PIPE, shell=True)
+		callbigscan.wait()
+		for line in callbigscan.stdout:
+			print line.strip()
+		time.sleep(120)
 		if ("cgis" in wordlists[1]):
-			time.sleep(30)
-			gobuster = "gobuster -u " + url + " -w " +  str(wordlists[1]) + " -s '200,204,301,302,307,403,500'"
-			os.system("gnome-terminal -e 'bash -c \"" + gobuster + "\";bash'")
-			
-	time.sleep(300) # Let's give Gobuster 5 minutes to complete or near completion before we hammer port 443 some more.
-	wfuzz(url)
+			print "[!] Starting gobuster cgi scan for " + url
+			gobuster = "gobuster -u " + url + " -w " +  str(wordlists[1]) + " -s '200,204,301,302,307,400,403'"
+			callcgiscan = subprocess.Popen(gobuster, stdout=subprocess.PIPE, shell=True)
+			callcgiscan.wait()
+			for line in callcgiscan.stdout:
+				print line.strip()
+			time.sleep(120)	
+			wfuzz(url)
 	
 def wfuzz(url):
-	print "[!] Starting wfuzz scan for " + url
 	if ("big" in wfuzzlist[0]):
-		time.sleep(15)
-		wfuzz = "wfuzz --hc 404,403 -c -z file," + str(wfuzzlist[0]) + " " + url + "/FUZZ"
-		os.system("gnome-terminal -e 'bash -c \"" + wfuzz + "\";bash'")
+		print "[!] Starting wfuzz big scan for " + url
+		wfuzz = "wfuzz --hc 404,403,400 -c -z file," + str(wfuzzlist[0]) + " " + url + "/FUZZ"
+		callbigscan = subprocess.Popen(wfuzz, stdout=subprocess.PIPE, shell=True)
+		callbigscan.wait()
+		for line in callbigscan.stdout:
+			print line.strip()
+		time.sleep(120)
 		if ("cgis" in wfuzzlist[1]):
-			time.sleep(30)
-			wfuzz = "wfuzz --hc 404,403 -c -z file," + str(wfuzzlist[1]) + " " + url + "/FUZZ"
-			os.system("gnome-terminal -e 'bash -c \"" + wfuzz + "\";bash'")
+			print "[!] Starting wfuzz cgi scan for " + url
+			wfuzz = "wfuzz --hc 404,403,400 -c -z file," + str(wfuzzlist[1]) + " " + url + "/FUZZ"
+			callcgiscan = subprocess.Popen(wfuzz, stdout=subprocess.PIPE, shell=True)
+			callcgiscan.wait()
+			for line in callcgiscan.stdout:
+				print line.strip()
+			time.sleep(120)
+			nikto(url)
 		
 def nikto(url):
 	cmd = "nikto -h " + url
 	os.system("gnome-terminal -e 'bash -c \"" + cmd + "\";bash'")
-	time.sleep(300) # Let's give Nikto 5 minutes to complete or near completion before we hammer port 443 some more.
-	gobuster(url)
-
+	
 def main():
-	nikto(url)
+	gobuster(url)
 
 if __name__=='__main__':
 	main()
