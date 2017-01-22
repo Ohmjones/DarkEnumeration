85b55c88dedc823f16964a67f207c362e472a5ec
Ohmjones <Shane.Jones@Ohmsecurities.com>
2017-01-21 21:46:59 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -2,6 +2,8 @@
 
 '''
 Homer Simpson is clearly a trademark/copyrighted entity from 'The Simpsons'. I don't wanna get any shit for using his drool face.
+In fact...
+	 The Simpsons TM & © 2017 Twentieth Century Fox Film Corporation. All rights reserved. 
 '''
 
 def intro():
