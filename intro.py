3e1572946fee7525b2b4bb1a063da6b6d8dd3123
Ohmjones <Shane.Jones@Ohmsecurities.com>
2017-02-27 11:07:15 -0500

    Update intro.py
    giving credit to Fox for Homer fucked the script up; so removed em.

--- intro.py
+++ intro.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python
 
-'''
-Homer Simpson is clearly a trademark/copyrighted entity from 'The Simpsons'. I don't wanna get any shit for using his drool face.
-In fact...
-	 The Simpsons TM & © 2017 Twentieth Century Fox Film Corporation. All rights reserved. 
-'''
 def intro():
 	print("""	    
 	      _ _,---._ 
